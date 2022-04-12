9f23ba2be899f9e6bf66726649d8112f8e90fea5
Yunsik Kim <yunsik0115@uos.ac.kr>
2022-04-12 12:44:53 +0900

    Variable Declaration in Javascript

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,23 @@
-let js = 'amazing';
-if (js === 'amazing') alert('javascript is fun!');
+let js = "amazing";
+if (js === "amazing") alert("javascript is fun!");
 console.log(40 * 8 + 23 - 10); // explicitly output the result to console.
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda"
+
+// homework
+// 1.
+let country = "South Korea";
+let continent = "Asia";
+let population = 51780000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// about single quotes and double quotes https://stackoverflow.com/questions/242813/when-should-i-use-double-or-single-quotes-in-javascript
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
