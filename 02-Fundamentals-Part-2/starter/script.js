12c9f8e2bf2536ebc8a08a8a0a20459a29a9d46a
Yunsik Kim <yunsik0115@uos.ac.kr>
2022-04-14 13:08:33 +0900

    Function Declaration and Function Expression (Hoisting later)

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,5 @@
 'use strict'; // has to be mentioned at the start of the script.
-// easier to avoid accidential error, show visible errors at the developmental phase.
+/*// easier to avoid accidential error, show visible errors at the developmental phase.
 
 let hasDriverLicense = false;
 const passTest = true;
@@ -37,5 +37,29 @@ console.log(appleJuice);
 console.log(fruitProcessor(5,0));
 
 const appleOrangeJuice = fruitProcessor(2,4);
-console.log(appleOrangeJuice);
+console.log(appleOrangeJuice);*/
+
+
+// function declaration
+function calcAge1(birthYear){
+    const age = 2037 - birthYear
+    return age;
+}
+
+// Declared function can be executed before the function decalration.
+// While function expression can't be executed before the expression.
+// => will learn about hoisting.
+
+const age1 = calcAge1(1991);
+console.log(age1);
+
+// function expression
+const calcAge2 = function (birthYear){
+    return 2037 - birthYear;
+}
+
+const age2 = calcAge2(1991);
+console.log(age2);
+
+/* Matter of personal Preference Expression vs Declaration */
 
