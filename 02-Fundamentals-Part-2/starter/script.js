0de2c8e671d377e88f93de32d0fe58df1d96dd3b
Yunsik Kim <yunsik0115@uos.ac.kr>
2022-04-14 12:50:04 +0900

    Use of the strict mode for avoiding accidential error

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,18 @@
+'use strict'; // has to be mentioned at the start of the script.
+// easier to avoid accidential error, show visible errors at the developmental phase.
+
+let hasDriverLicense = false;
+const passTest = true;
+if(passTest) // boolean function
+{
+    hasDriversLicense = true;
+}
+
+if(hasDriverLicense) // boolean function
+{
+    console.log("I can drive");
+}
+
+// const interface = 'Audio';
+// interface is the reserved word
+// const private = 534; // also the same
