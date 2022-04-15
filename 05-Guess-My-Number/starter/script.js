5a9de7941aed88f1d7bb8647cea91dd754fc2086
Yunsik Kim <yunsik0115@uos.ac.kr>
2022-04-15 16:43:45 +0900

    Handling the Events (OnClick)

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
+/*console.log(document.querySelector('.message').textContent);
 document.querySelector('.message');
 // if it was id we use hash
 // method that is available on document object.
@@ -12,3 +12,20 @@ document.querySelector('.score').textContent = 30;
 console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+const x = function () {
+  console.log(23);
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '❌No Number';
+  }
+}); // addEventListener(when to act, what to act)
+
+// Javascript will call function as soon as event happens.
