a756bf21b7cce46f3fa23338f93937c23221a936
Yunsik Kim <yunsik0115@uos.ac.kr>
2022-04-15 16:12:29 +0900

    Selecting and Manipulating Elements by using DOM

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message');
+// if it was id we use hash
+// method that is available on document object.
+// choose selector
+
+document.querySelector('.message').textContent = 'Correct Number!';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 30;
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
