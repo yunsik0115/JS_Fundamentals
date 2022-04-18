da49ac2a05ecd159a476f9a44e255f2636c6ce38
Yunsik Kim <yunsik0115@uos.ac.kr>
2022-04-18 10:51:06 +0900

    Game Resetting Implementation

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -6,8 +6,8 @@ const score1El = document.getElementById('score--1');
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 const diceEl = document.querySelector('.dice');
-const current0El = document.getElementById('current-0');
-const current1El = document.getElementById('current-1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -75,4 +75,21 @@ btnHold.addEventListener('click', function () {
       switchPlayerFunction();
     }
   }
+});
+
+btnNew.addEventListener('click', function () {
+  scores[0] = 0;
+  scores[1] = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+  playing = true;
+  currentScore = 0;
+  activePlayer = 0;
 });
