5b2f2cb167b3bd37a6f427146dc812343dc2ea68
Yunsik Kim <yunsik0115@uos.ac.kr>
2022-04-24 10:20:17 +0900

    Value Passing, Reference Passing Difference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -28,3 +28,37 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000); // undefined parameters will be set to default value
 */
+
+const flight = 'LH234';
+const jonas = {
+  name: 'jonas Schemdtmann',
+  passport: 24849646564,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999'; // not a great practice
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 24849646564) {
+    alert('check in');
+  } else {
+    alert('wrong passport!');
+  }
+};
+
+checkIn(flight, jonas);
+
+console.log(flight); // Still LH234
+// value calling
+console.log(jonas); // Manipluation af Passenger Object Value.
+// referencial calling
+
+// Is the same as doing...
+const flightNum = flight;
+const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random()) * 1000000000000;
+};
+newPassport(jonas);
+checkIn(flight, jonas);
