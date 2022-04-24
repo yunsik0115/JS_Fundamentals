e6ec5f8856bacc4db342b9413ba4ae340c67a668
Yunsik Kim <yunsik0115@uos.ac.kr>
2022-04-24 10:08:16 +0900

    default parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+/*const bookings = [];
+
+// Default Parameters ES6 Below
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // ES5
+  //numPassengers = numPassengers || 1;
+  // default parameter, if numPassengers doesn't exsist.
+  //price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+createBooking('LH123', undefined, 1000); // undefined parameters will be set to default value
+*/
