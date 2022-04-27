ef93cb006173ce9384731679fbc5f719c55f8b68
Yunsik Kim <yunsik0115@uos.ac.kr>
2022-04-27 14:38:27 +0900

    Implementation with filter & reduce methods

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -109,6 +109,13 @@ const createUsernames = function (accs) {
 createUsernames(accounts);
 // console.log(createUsernames('Steven Thomas Williams'));
 console.log(accounts);
+
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+
+calcDisplayBalance(account1.movements);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -251,3 +258,49 @@ const movementDescriptions = movements.map((mov, i, arr) => {
 });
 
 console.log(movementDescriptions);
+
+const deposits = movements.filter(function (value) {
+  return value > 0;
+});
+console.log(movements);
+console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
+
+console.log(movements);
+
+const balance = movements.reduce(function (
+  accumulator,
+  currentElementOfArray,
+  i,
+  arr
+) {
+  console.log(`Iteration ${i}: ${accumulator}`);
+  return accumulator + currentElementOfArray;
+},
+0); // 0 -> initial value for iteration (accumulation)
+console.log(balance);
+
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
+
+/*
+const balance = movements.reduce((acc, cur) => acc + cur), 0);
+ */
+
+// Maximum Value -> can be reduced by reduce
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) {
+    return acc;
+  } else {
+    return mov;
+  }
+  // traverse array if acc indicates small number than mov mov will be retured as acc
+}, movements[0]);
+console.log(max);
