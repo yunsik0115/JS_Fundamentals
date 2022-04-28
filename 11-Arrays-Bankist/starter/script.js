c7955d9fe8a1a1a9687714729266b8f05c71fa61
Yunsik Kim <yunsik0115@uos.ac.kr>
2022-04-28 15:20:45 +0900

    Using findIndex Method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -199,6 +199,29 @@ btnTransfer.addEventListener('click', function (e) {
   }
   updateUI(currentAccount);
 });
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    console.log(index);
+
+    //Delete Account
+    accounts.splice(index, 1);
+    // indexof we can find
+
+    //Hide UI
+    containerApp.style.opacity = 0;
+  }
+
+  inputCloseUsername.value = inputClosePin.value = '';
+});
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
